80557d97720ca3f6450d2eb1b8d56e2835f1e792
sophelios <rick.bruyere@gmail.com>
2016-08-08 17:42:23 -0400

    First Commit

---
+++ StreamReaderClssWrk/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+namespace StreamReaderClssWrk
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string line;
+            StreamReader shakespeare = new StreamReader(@"..\..\Shakespeare.txt");
+            line = shakespeare.ReadLine();
+            Console.WriteLine(line);
+            Console.WriteLine();
+            shakespeare.Close();
+
+            StreamReader shakespeare2 = new StreamReader(@"..\..\Shakespeare.txt");
+            line = shakespeare2.ReadToEnd();
+            Console.WriteLine(line);
+            Console.WriteLine();
+            shakespeare2.Close();
+
+            StreamReader shakespeare3 = new StreamReader(@"..\..\Shakespeare.txt");
+            line = shakespeare3.ReadToEnd();
+            foreach (char c in line)
+            {
+                string changeUp = c.ToString();
+                if (c == 'a' || c == 'e' || c == 'i' || c == 'o' || c == 'u')
+                {
+                    changeUp = c.ToString().ToUpper();
+                }
+                Console.Write(changeUp);
+
+            }
+            shakespeare3.Close();
+        }
+    }
+}
